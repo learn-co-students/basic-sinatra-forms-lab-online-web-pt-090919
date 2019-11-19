dbf5eb166adb7796d73bc8368a8adaebf613596a
Naya Willis <greedybrain18@gmail.com>
2019-11-19 17:03:39 -0500

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,20 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+    get '/newteam' do 
+        erb :newteam # get form from newteam.erb
+    end
+
+    post '/team' do # then show form input data from ^ here
+        @name = params[:name]
+        @coach = params[:coach]
+        @pg = params[:pg]
+        @sg = params[:sg]
+        @pf = params[:pf]
+        @sf = params[:sf]
+        @c = params[:c]
+
+        erb :team
+    end
 
 end
