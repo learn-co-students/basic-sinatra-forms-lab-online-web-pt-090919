fa1d7b3120db9dc1c7d035aca94c83e1420daefc
Edward Heaver <heaver.edward02@gmail.com>
2019-12-15 15:41:39 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,15 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+  
+  get "/newteam" do 
+    erb :newteam
+  end
+  
+  post "/team" do
+    #@team_name = params[:team_name]
+    erb :team
+  end
 
 
 end
